e5a6395cd91c733e21046e55ef91085208fe131e
Kamily dos Santos <102933216+KamilydosSantos@users.noreply.github.com>
2024-02-19 20:48:57 -0300

    feat: json server data and dynamic icons

---
+++ src/components/icons/DynamicIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="iconName" />
+</template>
+
+<script>
+export default {
+  name: "DynamicIcon",
+  props: {
+    iconName: String
+  },
+  components: {
+    HtmlIcon: () => import("./HtmlIcon.vue"),
+    CssIcon: () => import("./CssIcon.vue"),
+    JavaScriptIcon: () => import("./JavaScriptIcon.vue"),
+    VueJsIcon: () => import("./VueJsIcon.vue"),
+  }
+}
+</script>
+
+<style>
+
+</style>
